da30c6860e79629be229aa0ed4e7d80dd819eb12
Izzat_Melikov <gagarin.yurick@mail.ru>
2025-10-13 21:59:34 +0300

    Finish DataAccess

--- App/Backend/CRM/CRM.Application/obj/Debug/net9.0/CRM.Application.AssemblyInfo.cs
+++ App/Backend/CRM/CRM.Application/obj/Debug/net9.0/CRM.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a322f52c35093ef23887309b102eea3ffeecded4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bcac27aefcf2dfc4b678118944bff4e49c80ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
